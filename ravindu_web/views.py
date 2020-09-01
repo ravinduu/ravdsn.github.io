82c61ec05f91fa0519a08f271824c874e5fc7ab6
ravindu <ravindu.dilshan.rd@gmail.com>
2020-09-01 09:32:41 +0530

    add views

--- ravindu_web/views.py
+++ ravindu_web/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1> Home </h1>')
+
+def posts(request):
+    return HttpResponse('<h1> Posts </h1>')
+
+def post(request):
+    return HttpResponse('<h1> post </h1>')
+    
+def profile(request):
+    return HttpResponse('<h1> profile </h1>')
